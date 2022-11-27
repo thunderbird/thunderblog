eaf038435e8f5b410ac5062f1326f67d31a5ef7d
Andreas Müller <mail@devmount.de>
2022-11-27 21:31:07 +0100

    ➕ social: telegram

--- parts/post-social.php
+++ parts/post-social.php
@@ -23,6 +23,19 @@
 			<polyline points="3 7 12 13 21 7" />
 		</svg>
 	</a>
+	<!-- Twitter -->
+	<a
+		href="https://twitter.com/intent/tweet?original_referer=<?php echo urlencode( get_site_url() ); ?>&text=<?php echo urlencode( get_the_title() ); ?>&url=<?php echo urlencode(get_permalink()) ?>&via=mozthunderbird"
+		class="social-twitter"
+		target="_blank"
+		title="<?php _e( 'Share this article on Twitter', 'thunderblog' ); ?>"
+		aria-label="<?php _e( 'Share this article on Twitter', 'thunderblog' ); ?>"
+	>
+		<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-m" viewBox="0 0 24 24">
+			<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+			<path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" />
+		</svg>
+	</a>
 	<!-- WhatsApp -->
 	<a
 		href="https://api.whatsapp.com/send?phone=&text=<?php echo get_the_title() ?> <?php echo get_permalink() ?>"
@@ -37,17 +50,17 @@
 			<path d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1" />
 		</svg>
 	</a>
-	<!-- Twitter -->
+	<!-- Telegram -->
 	<a
-		href="https://twitter.com/intent/tweet?original_referer=<?php echo urlencode( get_site_url() ); ?>&text=<?php echo urlencode( get_the_title() ); ?>&url=<?php echo urlencode(get_permalink()) ?>&via=mozthunderbird"
-		class="social-twitter"
+		href="https://telegram.me/share/url?url=<?php echo get_permalink() ?>&text=<?php echo get_the_title() ?>"
+		class="social-telegram"
 		target="_blank"
-		title="<?php _e( 'Share this article on Twitter', 'thunderblog' ); ?>"
-		aria-label="<?php _e( 'Share this article on Twitter', 'thunderblog' ); ?>"
+		title="<?php _e( 'Share this article via Telegram', 'thunderblog' ); ?>"
+		aria-label="<?php _e( 'Share this article via Telegram', 'thunderblog' ); ?>"
 	>
 		<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-m" viewBox="0 0 24 24">
 			<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-			<path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" />
+			<path d="M15 10l-4 4l6 6l4 -16l-18 7l4 2l2 6l3 -4" />
 		</svg>
 	</a>
 	<!-- LinkedIn -->
