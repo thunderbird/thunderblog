b14f159fc8251a56258641a5486f94c508084db7
Andrei Hajdukewycz <sancus@off.net>
2023-08-11 19:39:13 +0000

    ✅ 1.0.12

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
  */
 
 if ( ! defined( 'THUNDERBLOG_VERSION' ) ) {
-	define( 'THUNDERBLOG_VERSION', '1.0.11' );
+	define( 'THUNDERBLOG_VERSION', '1.0.12' );
 }
 
 // enable theme support for featured images
