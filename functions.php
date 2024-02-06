ff714b35266b1c6cd585850494d60ccc449a41fc
Melissa Autumn <melissa@thunderbird.net>
2024-02-06 08:48:01 -0800

    ✅ 1.0.13

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@
  */
 
 if ( ! defined( 'THUNDERBLOG_VERSION' ) ) {
-	define( 'THUNDERBLOG_VERSION', '1.0.12' );
+	define( 'THUNDERBLOG_VERSION', '1.0.13' );
 }
 
 // enable theme support for featured images
