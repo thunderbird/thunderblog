e0bfec3f475a7b08033cceb1141ad0d265ad77bc
Andreas <mail@devmount.de>
2024-06-12 21:44:03 +0200

    Custom logo for header and footer (#62)

--- parts/logo.php
+++ parts/logo.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * Displays the linked logo brand.
+ * A custom logo can be set via customizer > site identity > logo.
+ * If no custom logo is set, a default logo from assets will be used.
  *
  * @package WordPress
  * @subpackage Thunderblog
@@ -10,5 +12,9 @@
 ?>
 
 <a class="logo" href="<?php echo site_url(); ?>">
-	<img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/thunderbird_logo.svg" alt="Logo" />
+	<?php if ( has_custom_logo() ) : ?>
+		<?php the_custom_logo(); ?>
+	<?php else: ?>
+		<img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/thunderbird_logo.svg" alt="Logo" />
+	<?php endif; ?>
 </a>
